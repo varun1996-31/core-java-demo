9e6b6e052b2c185ce7944bb53f2678d1f1379d50
feature-varun <pratapvarun21@gmail.com>
2022-11-17 20:48:39 +0530

    varun-project-percentage

---
+++ src/Gittes/Percentage.java
@@ -0,0 +1,20 @@
+package Gittes;
+
+public class Percentage {
+    public static void main( String[]args){
+         int math=76;
+         int science=81;
+         int english=77;
+         int hindi=82;
+         int phyedu=78;
+          double total=math+science+english+hindi+phyedu;
+          double percentage=total/500*100;
+          System.out.println("percentage score by the candiate is"+percentage);
+          double number=7696;
+          double per=12;
+          double result=number*per/100;
+          System.out.println("the 12 percentage of the number is "+result);
+          int cube= hindi*hindi*hindi;
+          System.out.println("the cube of the number is "+cube);
+    }
+}
